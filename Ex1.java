d715e182d24c5e2154f10b391a224aab70148179
sachin bhatt <dasnadas208@gmail.com>
2023-02-14 00:47:44 +0000

    changes

--- Ex1.java
+++ Ex1.java
@@ -2,6 +2,6 @@ package homework;
 
 public class Ex1 {
     public static void main (String[] args){
-        System.out.println("welcome to java program");
+        System.out.println("welcome to java program123");
     }
 }
